f41b84b6931c9d97f3e4997df5739019d7cbf573
Oliver Short <oshort55@gmail.com>
2016-04-18 11:28:19 -0400

    Initial Commit

---
+++ Scientific/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Scientific
+//
+//  Created by Oliver Short on 4/18/16.
+//  Copyright © 2016 Oliver Short. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Scientific/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Scientific
+//
+//  Created by Oliver Short on 4/18/16.
+//  Copyright © 2016 Oliver Short. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
